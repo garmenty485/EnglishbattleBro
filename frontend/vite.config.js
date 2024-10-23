78db7695a3176b857d36ae0975e1e23608a486cc
garmenty485 <garmenty485@gmail.com>
2024-10-23 00:47:18 -0700

    vite config增加BACKEND_URL以便未來有domain時較為方便

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,12 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+import dotenv from 'dotenv';
+dotenv.config({ path: '../backend/.env' });
+
+const backendUrl = process.env.BACKEND_URL || 'http://localhost:5000';
+
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: backendUrl,
         changeOrigin: true
       }
     }
