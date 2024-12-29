0a9ad15fa7226f17cb6e8bf6d1b9989cc216dd04
garmenty485 <garmenty485@gmail.com>
2024-12-28 21:16:02 -0800

    刪除無用的根目錄的.env

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-import dotenv from 'dotenv';
-dotenv.config({ path: '../backend/.env' });
-
-const backendUrl = process.env.BACKEND_URL || 'http://localhost:5000';
-
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: backendUrl,
+        target: 'http://localhost:5000',
         changeOrigin: true
       }
     }
